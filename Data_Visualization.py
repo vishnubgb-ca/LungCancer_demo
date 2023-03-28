c6c02ffe6c2b56e8c476c15b8101e1119402fa4a
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-03-28 11:53:21 +0530

    Update Data_Visualization.py

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -10,12 +10,16 @@ def data_visualization():
         if (len(dataset[col].unique()) > 5) and (col != 'Level'):
             fig,ax = plt.subplots(1,1, figsize=(5,4))
             sns.distplot(x=dataset[col][1:])
+            plt.show()
         elif col != 'Level':
             dataset[col] != 'Level'
             fig,ax = plt.subplots(1,1, figsize = (5,4))
             sns.countplot(x=dataset[col][1:])
+            plt.show()
     sns.pairplot(dataset)
+    plt.show()
     cor_mat = dataset.corr()
     fig = plt.figure(figsize=(15,7))
     sns.heatmap(cor_mat,annot=True)
+    plt.show()
     return dataset
