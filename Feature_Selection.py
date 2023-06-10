9939220ead428eb999de3cb601cd28cada9fd91b
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-06-10 08:54:39 +0530

    Update Feature_Selection.py

--- Feature_Selection.py
+++ Feature_Selection.py
@@ -1,5 +1,5 @@
 from Feature_Engineering import feature_engineering
-from dvc import dvc
+#from dvc import dvc
 
 def feature_selection():
     dataset = feature_engineering()
@@ -7,8 +7,8 @@ def feature_selection():
     dataset = dataset.drop(["patient_id", "index"], axis =1)    
     print(dataset.head())
     dataset.to_csv('cleaned.csv')
-    print("DVC process start")
-    dvc()
+    #print("DVC process start")
+    #dvc()
     return dataset
 
 feature_selection()
