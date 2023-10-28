4e1ef423a1328c5e04ffed4925c379c1b5576cc8
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2023-10-28 14:26:05 +0530

    Update Feature_Engineering.py

--- Feature_Engineering.py
+++ Feature_Engineering.py
@@ -7,8 +7,8 @@ def feature_engineering():
         print(col, len(dataset[col].unique()))
     dataset['level'] = le.fit_transform(dataset.level)
     print(dataset.head())
-    dataset = dataset.drop(["patient_id", "index"], axis =1)    
-    print(dataset.head())
+    # dataset = dataset.drop(["patient_id", "index"], axis =1)    
+    # print(dataset.head())
     dataset.to_csv('cleaned.csv')
     return dataset
 feature_engineering()
